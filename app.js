80bbdb29119c85eed71ee48073e830fe26f0a78e
Mehul <yourdevmehul@gmail.com>
2025-06-29 16:28:10 +0530

    Styling completed using bootstrap

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 8080;
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 //connect to mongodb
 main()
@@ -15,9 +16,12 @@ async function main(){
     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
+app.engine("ejs",ejsMate);
+
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views")); 
 
+app.use(express.static(path.join(__dirname,"/public")));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
--- models/listing.js
+++ models/listing.js
@@ -12,10 +12,10 @@ let listingSchema = new Schema({
         filename: String,
         url: {
             type: String,
-            default: "https://unsplash.com/photos/outdoor-lamps-turned-on-XbwHrt87mQ0",
+            default: "https://images.unsplash.com/photo-1519046904884-53103b34b206?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             set: (v) =>
             v === ""
-            ? "https://unsplash.com/photos/outdoor-lamps-turned-on-XbwHrt87mQ0"
+            ? "https://images.unsplash.com/photo-1519046904884-53103b34b206?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             : v,
         },
     },
