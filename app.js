a3225fbc3181665a86cd30c6ac934a997896a783
Mehul <yourdevmehul@gmail.com>
2025-07-08 19:56:32 +0530

    Phase 1 part c completed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsyn.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 
 //connect to mongodb
 main()
@@ -31,7 +32,22 @@ app.get("/", (req, res) => {
     res.send("Working");
 });
 
-app.get("/testListing", async (req, res) => {
+//listing validation function
+
+const validateListing = (req,res,next) =>{
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(400, errMsg);
+
+    }
+    else{
+        next();
+    }
+    
+}
+
+app.get("/testListing", wrapAsync(async (req, res) => {
     let sampleListing = new Listing({
         title: "villa",
         description: "By the beach",
@@ -44,27 +60,26 @@ app.get("/testListing", async (req, res) => {
     console.log("sample was saved");
     res.send("Successful");
 
-});
+}));
 
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("./listings/index.ejs", { allListings });
-});
+}));
 
 app.get("/listings/new", (req, res) => {
     res.render("./listings/new.ejs")
 });
 
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/show.ejs", { listing });
-});
+}));
 
 //create route
-app.post("/listings",
+app.post("/listings",validateListing,
     wrapAsync(async (req, res) => {
-
         const newListing = new Listing(req.body.listing);
         await newListing.save();
         console.log(newListing);
@@ -72,35 +87,36 @@ app.post("/listings",
 
     }));
 
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/edit.ejs", { listing });
-});
+}));
 
-app.patch("/listings/:id", async (req, res) => {
+app.patch("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect("/listings");
-});
+}));
 
-app.delete("/listings/:id", async (req, res) => {
+app.delete("/listings/:id",wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect("/listings");
-});
+}));
 
 // This is your catch-all route for unhandled paths
 // It MUST come after all your defined routes
-app.all("*", (req, res, next) => {
+app.use((req, res, next) => {
     next(new ExpressError(404, "Page not found!"));
 });
 
 // This is your error-handling middleware
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong!" } = err; // Set default values for statusCode and message
-    res.status(statusCode).send(message);
+    // res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs",{err})
 });
 
 app.listen(port, () => {
