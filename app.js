01f34c4fb2321c4c1d3a6b4c667a824b8fe964b4
Mehul <yourdevmehul@gmail.com>
2025-07-25 00:09:29 +0530

    Project phase3 part c complete

--- app.js
+++ app.js
@@ -52,9 +52,9 @@ const sessionOptions = {
     }
 };
 
-app.get("/", (req, res) => {
-    res.send("Working");
-});
+// app.get("/", (req, res) => {
+//     res.send("Working");
+// });
 
 //project phase2 partc
 app.use(session(sessionOptions));
