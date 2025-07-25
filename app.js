75738767fa02f35f9cee667b4b2762e4a2a1f7be
Mehul <yourdevmehul@gmail.com>
2025-07-25 12:26:42 +0530

    minor changes:

--- app.js
+++ app.js
@@ -108,6 +108,9 @@ app.use((req,res,next)=>{
 //     let registeredUser = await User.register(fakeUser,"abcd");
 //     res.send(registeredUser);
 // });
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+})
 
 
 //listing routes
